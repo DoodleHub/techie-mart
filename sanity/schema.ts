ad732948dfe876a8b0d5dfd704d76ff0f9536285
Chao Chen <chaochen@live.com>
2024-07-04 01:51:16 -0400

    chore: add product and banner sanity schema

--- sanity/schema.ts
+++ sanity/schema.ts
@@ -1,5 +1,8 @@
-import { type SchemaTypeDefinition } from 'sanity'
+import { type SchemaTypeDefinition } from 'sanity';
+
+import product from './schemas/product';
+import banner from './schemas/banner';
 
 export const schema: { types: SchemaTypeDefinition[] } = {
-  types: [],
-}
+  types: [product, banner],
+};
